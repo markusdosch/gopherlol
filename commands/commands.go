c5f2c0ec43c1d027523950e721753c2e8eaade6f
brae <>
2023-06-20 21:25:19 -0400

    [ clean ] fix the imports and modify the main file

--- commands/commands.go
+++ commands/commands.go
@@ -7,11 +7,6 @@ import (
 
 type Commands struct{}
 
-func (c *Commands) Help() {
-	// We want this method to show up in `help`/`list` results
-	// But the actual logic for these commands is in `main.go`
-}
-
 func (c *Commands) List() {
 	// We want this method to show up in `help`/`list` results
 	// But the actual logic for these commands is in `main.go`
@@ -19,10 +14,6 @@ func (c *Commands) List() {
 
 func (c *Commands) G(cmdArg string) string {
 	return fmt.Sprintf("https://www.google.com/#q=%s", url.QueryEscape(cmdArg))
-}
-
-func (c *Commands) Author() string {
-	return "https://www.markusdosch.com"
 }
 
 func (c *Commands) So(cmdArg string) string {
